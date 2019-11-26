c334d7c385b6f21f5ff0d0f24ea3c04930057e3e
Veer275# <raghuveer.mh@cognitiveclouds.com>
2019-11-27 01:35:20 +0530

    One Line Commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ let array = []
 let check = []
 
 const tictactoe = (id) => {
-  let count = 0
   document.getElementById(id).disabled = true
   let matchArray = ['123', '456', '789', '147', '258', '369', '159', '357']
   if (array.length < 9) {
@@ -14,22 +13,38 @@ const tictactoe = (id) => {
     check.push({ X: id })
     document.getElementById(id).innerHTML = 'X'
     let newArr = arr.filter(a => !array.includes(a))
-    let other = newArr.includes(id + 1) ? (id + 1) : newArr[parseInt(Math.round(Math.random() * (newArr.length - 1)))]
+    let other = newArr.includes(5) ? 5 : newArr[parseInt(Math.round(Math.random() * (newArr.length - 1)))]
+    let count = 0
     check.map(num => {
       num.X && count++
-      if (count > 0) {
-        matchArray.map(mar => {
-          if (document.getElementById(mar[0]).innerHTML !== null && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[2]).innerHTML.length === 0) {
+      if (count > 1) {
+        for(let i = 0; i < matchArray.length; i++ ) {
+          let mar = matchArray[i]
+          if (document.getElementById(mar[0]).innerHTML.length !== 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[2]).innerHTML.length === 0) {
             other = mar[2]
+            break;
           }
-          else if (document.getElementById(mar[0]).innerHTML !== null && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML && document.getElementById(mar[1]).innerHTML.length === 0) {
+          else if (document.getElementById(mar[0]).innerHTML.length !== 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML && document.getElementById(mar[1]).innerHTML.length === 0) {
             other = mar[1]
+            break;
           }
-          else if (document.getElementById(mar[1]).innerHTML !== null && document.getElementById(mar[1]).innerHTML === document.getElementById(mar[2]).innerHTML && document.getElementById(mar[0]).innerHTML.length === 0) {
+          else if (document.getElementById(mar[1]).innerHTML.length !== 0 && document.getElementById(mar[1]).innerHTML === document.getElementById(mar[2]).innerHTML && document.getElementById(mar[0]).innerHTML.length === 0) {
             other = mar[0]
+            break;
           }
-          return null
-        })
+          else if (document.getElementById(mar[1]).innerHTML.length !== 0 && document.getElementById(mar[1]).innerHTML === document.getElementById(mar[0]).innerHTML && document.getElementById(mar[2]).innerHTML.length === 0) {
+            other = mar[2]
+            break;
+          }
+          else if (document.getElementById(mar[2]).innerHTML.length !== 0 && document.getElementById(mar[2]).innerHTML === document.getElementById(mar[0]).innerHTML && document.getElementById(mar[1]).innerHTML.length === 0) {
+            other = mar[1]
+            break;
+          }
+          else if (document.getElementById(mar[2]).innerHTML.length !== 0 && document.getElementById(mar[2]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML.length === 0) {
+            other = mar[0]
+            break;
+          }
+        }
       }
       return null
     })
@@ -43,15 +58,20 @@ const tictactoe = (id) => {
     if (document.getElementById(mar[0]).innerHTML.length > 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML) {
       document.getElementById('none').style.display = 'block'
       document.getElementById('noneText').style.display = 'block'
-      document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML + ' WON')
+      document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML === 'X' ? 'WINNER' : 'LOSER')
     }
     return null
   })
+  if (array.length > 8) {
+      document.getElementById('none').style.display = 'block'
+      document.getElementById('noneText').style.display = 'block'
+      document.getElementById('noneText').innerHTML = 'DRAW'
+  }
 }
 function App() {
   return (
     <div>
-      <h1>My Tic Tac Toe Game</h1>
+      <span className='header'>Tic Tac Toe</span>
       <div className='grid-box'>
         {arr.map(item =>
           <button onClick={() => tictactoe(item)} id={item} key={item} />
@@ -60,6 +80,7 @@ function App() {
       <button className='refresh' onClick={() => window.location.reload()}>RESET GAME</button>
       <span id='none' style={{ display: 'none' }} onClick={() => window.location.reload()} />
       <span id='noneText' style={{ display: 'none' }} onClick={() => window.location.reload()} />
+      <i className = 'watermark'>Raghuveer Bharadwaj</i>
     </div>
   )
 }
