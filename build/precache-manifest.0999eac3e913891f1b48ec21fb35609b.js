02f272299036077a0f57f52aa771ac154e9a4d21
Veer275# <raghuveer.mh@cognitiveclouds.com>
2019-11-26 10:03:00 +0530

    One Line Commit

---
+++ build/precache-manifest.0999eac3e913891f1b48ec21fb35609b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65edaedca8cb1efe0bb522912e0b93e",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "e9e547a360a91f11fa3b",
+    "url": "/Tic-Tac-Toe/static/css/main.9c95b08f.chunk.css"
+  },
+  {
+    "revision": "05b11ca5f991a42b4156",
+    "url": "/Tic-Tac-Toe/static/js/2.d457cd61.chunk.js"
+  },
+  {
+    "revision": "e9e547a360a91f11fa3b",
+    "url": "/Tic-Tac-Toe/static/js/main.7affcbd4.chunk.js"
+  },
+  {
+    "revision": "1e2556c61a8061438697",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.c58eff42.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let array = []
+
+const tictactoe = (id) => {
+  document.getElementById(id).disabled = true
+  if (array.length < 9) {
+      array.push(id)
+      document.getElementById(id).innerHTML = 'X'
+      let other = arr.filter(a => !array.includes(a))[parseInt(Math.round(Math.random() * arr.filter(a => !array.includes(a)).length))]
+      console.log(other)
+      if (other) {
+        array.push(other)
+        document.getElementById(other).disabled = true
+        document.getElementById(other).innerHTML = 'O'
+      }
+  }
+  let matchArray = ['123', '456', '789', '147', '258', '369', '159', '357']
+  matchArray.map(mar => {
+    if (document.getElementById(mar[0]).innerHTML.length > 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML)
+    {
+      document.getElementById('none').style.display = 'block'
+    document.getElementById('noneText').style.display = 'block'
+    document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML + ' WON')
+  }
+  })
+}
+function App() {
+  return (
+    <div>
+      <h1>My Tic Tac Toe Game</h1>
+      <div className='grid-box'>
+        {arr.map(item =>
+          <button onClick={() => tictactoe(item)} id={item} key={item} />
+        )}
+      </div>
+      <button className='refresh' onClick={() => window.location.reload()}>RESET GAME</button>
+      <span id='none' style={{display: 'none'}} onClick={()=>window.location.reload()} />
+      <span id='noneText' style={{display: 'none'}} onClick={()=>window.location.reload()}/>
+    </div>
+  )
+}
+
+export default App;
