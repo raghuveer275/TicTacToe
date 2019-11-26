0431e6dca61a322c0bae93f62967fb4b7bc40057
Veer275# <raghuveer.mh@cognitiveclouds.com>
2019-11-26 23:22:12 +0530

    One Line Commit

--- src/App.js
+++ src/App.js
@@ -3,28 +3,49 @@ import './App.css';
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 let array = []
+let check = []
 
 const tictactoe = (id) => {
+  let count = 0
   document.getElementById(id).disabled = true
-  if (array.length < 9) {
-      array.push(id)
-      document.getElementById(id).innerHTML = 'X'
-      let newArr = arr.filter(a => !array.includes(a))
-      let other = newArr[parseInt(Math.round(Math.random() * (newArr.length -1)))]
-      if (other) {
-        array.push(other)
-        document.getElementById(other).disabled = true
-        document.getElementById(other).innerHTML = 'O'
-      }
-  }
   let matchArray = ['123', '456', '789', '147', '258', '369', '159', '357']
-  matchArray.map(mar => {
-    if (document.getElementById(mar[0]).innerHTML.length > 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML)
-    {
-      document.getElementById('none').style.display = 'block'
-    document.getElementById('noneText').style.display = 'block'
-    document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML + ' WON')
+  if (array.length < 9) {
+    array.push(id)
+    check.push({ X: id })
+    document.getElementById(id).innerHTML = 'X'
+    let newArr = arr.filter(a => !array.includes(a))
+    let other = newArr.includes(id + 1) ? (id + 1) : newArr[parseInt(Math.round(Math.random() * (newArr.length - 1)))]
+    check.map(num => {
+      num.X && count++
+      if (count > 0) {
+        matchArray.map(mar => {
+          if (document.getElementById(mar[0]).innerHTML !== null && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[2]).innerHTML.length === 0) {
+            other = mar[2]
+          }
+          else if (document.getElementById(mar[0]).innerHTML !== null && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML && document.getElementById(mar[1]).innerHTML.length === 0) {
+            other = mar[1]
+          }
+          else if (document.getElementById(mar[1]).innerHTML !== null && document.getElementById(mar[1]).innerHTML === document.getElementById(mar[2]).innerHTML && document.getElementById(mar[0]).innerHTML.length === 0) {
+            other = mar[0]
+          }
+          return null
+        })
+      }
+      return null
+    })
+    if (other) {
+      array.push(other)
+      document.getElementById(other).disabled = true
+      document.getElementById(other).innerHTML = 'O'
+    }
   }
+  matchArray.map(mar => {
+    if (document.getElementById(mar[0]).innerHTML.length > 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML) {
+      document.getElementById('none').style.display = 'block'
+      document.getElementById('noneText').style.display = 'block'
+      document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML + ' WON')
+    }
+    return null
   })
 }
 function App() {
@@ -37,8 +58,8 @@ function App() {
         )}
       </div>
       <button className='refresh' onClick={() => window.location.reload()}>RESET GAME</button>
-      <span id='none' style={{display: 'none'}} onClick={()=>window.location.reload()} />
-      <span id='noneText' style={{display: 'none'}} onClick={()=>window.location.reload()}/>
+      <span id='none' style={{ display: 'none' }} onClick={() => window.location.reload()} />
+      <span id='noneText' style={{ display: 'none' }} onClick={() => window.location.reload()} />
     </div>
   )
 }
