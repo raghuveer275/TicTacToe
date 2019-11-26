589c96793e7b440f2f3780c6bb9f665a8bee9fff
Veer275# <raghuveer.mh@cognitiveclouds.com>
2019-11-26 10:05:14 +0530

    Updates

---
+++ precache-manifest.87a04ec8f8a907cd10c7e3d2e9c6bf50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1656024dc2268bc014af20bb523cf41",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "e45817506004477e95e5",
+    "url": "/Tic-Tac-Toe/static/css/main.fa5baa67.chunk.css"
+  },
+  {
+    "revision": "53f8473022fbaa1fbe48",
+    "url": "/Tic-Tac-Toe/static/js/2.c4539dc2.chunk.js"
+  },
+  {
+    "revision": "e45817506004477e95e5",
+    "url": "/Tic-Tac-Toe/static/js/main.26b21bac.chunk.js"
+  },
+  {
+    "revision": "579806e3dbaa9156fc34",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.935b14f8.js"
+  }
+]);
