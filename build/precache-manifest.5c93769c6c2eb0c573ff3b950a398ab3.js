d0a5672e278a9df25015ce4678c5cd390c350ff7
Veer275# <raghuveer.mh@cognitiveclouds.com>
2019-12-02 18:12:00 +0530

    One Line Commit

---
+++ build/precache-manifest.5c93769c6c2eb0c573ff3b950a398ab3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458134dd07c831b96b408c5b0fd64552",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "14dc1bbb4d824c0fb278",
+    "url": "/Tic-Tac-Toe/static/css/main.db73054c.chunk.css"
+  },
+  {
+    "revision": "0a4e84612a5651937d75",
+    "url": "/Tic-Tac-Toe/static/js/2.17053863.chunk.js"
+  },
+  {
+    "revision": "14dc1bbb4d824c0fb278",
+    "url": "/Tic-Tac-Toe/static/js/main.aa89bad1.chunk.js"
+  },
+  {
+    "revision": "579806e3dbaa9156fc34",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.935b14f8.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,88 +1,369 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.scss';
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let matchArray = ['123', '456', '789', '147', '258', '369', '159', '357']
 let array = []
 let check = []
+let count = 0
 
-const tictactoe = (id) => {
-  document.getElementById(id).disabled = true
-  let matchArray = ['123', '456', '789', '147', '258', '369', '159', '357']
-  if (array.length < 9) {
-    array.push(id)
-    check.push({ X: id })
-    document.getElementById(id).innerHTML = 'X'
-    let newArr = arr.filter(a => !array.includes(a))
-    let other = newArr.includes(5) ? 5 : newArr[parseInt(Math.round(Math.random() * (newArr.length - 1)))]
-    let count = 0
-    check.map(num => {
-      num.X && count++
-      if (count > 1) {
-        for(let i = 0; i < matchArray.length; i++ ) {
-          let mar = matchArray[i]
-          if (document.getElementById(mar[0]).innerHTML.length !== 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[2]).innerHTML.length === 0) {
+export class App extends Component {
+
+  easy = (id) => {
+    this.setState({ reset: true })
+    count++
+    document.getElementById(id).disabled = true
+    if (array.length < 9) {
+      array.push(id)
+      document.getElementById(id).innerHTML = 'X'
+      let newArr = arr.filter(a => !array.includes(a))
+      let other = newArr[parseInt(Math.round(Math.random() * (newArr.length - 1)))]
+      for (let i = 0; i < matchArray.length; i++) {
+        let mar = matchArray[i]
+        let a = document.getElementById(mar[0]).innerHTML
+        let b = document.getElementById(mar[1]).innerHTML
+        let c = document.getElementById(mar[2]).innerHTML
+        if (a === 'O' && a === b && c.length === 0) {
+          other = mar[2]
+          break;
+        }
+        else if (a === 'O' && a === c && b.length === 0) {
+          other = mar[1]
+          break;
+        }
+        else if (b === 'O' && b === c && a.length === 0) {
+          other = mar[0]
+          break;
+        }
+        else if (b === 'O' && b === a && c.length === 0) {
+          other = mar[2]
+          break;
+        }
+        else if (c === 'O' && c === a && b.length === 0) {
+          other = mar[1]
+          break;
+        }
+        else if (c === 'O' && c === b && a.length === 0) {
+          other = mar[0]
+          break;
+        }
+      }
+      if (other) {
+        array.push(other)
+        document.getElementById(other).disabled = true
+        document.getElementById(other).innerHTML = 'O'
+      }
+    }
+    if (array.length > 8) {
+      document.getElementById('none').style.display = 'block'
+      document.getElementById('noneText').style.display = 'block'
+      document.getElementById('noneText').innerHTML = 'DRAW'
+    }
+    matchArray.map(mar => {
+      if (document.getElementById(mar[0]).innerHTML.length > 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML) {
+        document.getElementById('none').style.display = 'block'
+        document.getElementById('noneText').style.display = 'block'
+        document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML === 'O' && 'LOSER')
+      }
+      return null
+    })
+    matchArray.map(mar => {
+      if (document.getElementById(mar[0]).innerHTML.length > 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML) {
+        document.getElementById('none').style.display = 'block'
+        document.getElementById('noneText').style.display = 'block'
+        document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML === 'X' ? 'WINNER' : 'LOSER')
+      }
+      return null
+    })
+  }
+
+  hard = (id) => {
+    document.getElementById(id).disabled = true
+    if (array.length < 9) {
+      array.push(id)
+      check.push({ X: id })
+      document.getElementById(id).innerHTML = 'X'
+      let newArr = arr.filter(a => !array.includes(a))
+      let other = newArr.includes(5) ? 5 : newArr[parseInt(Math.round(Math.random() * (newArr.length - 1)))]
+      let count = 0
+      check.map(num => {
+        num.X && count++
+        if (count > 1) {
+          for (let i = 0; i < matchArray.length; i++) {
+            let mar = matchArray[i]
+            if (document.getElementById(mar[0]).innerHTML.length !== 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[2]).innerHTML.length === 0) {
+              other = mar[2]
+              break;
+            }
+            else if (document.getElementById(mar[0]).innerHTML.length !== 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML && document.getElementById(mar[1]).innerHTML.length === 0) {
+              other = mar[1]
+              break;
+            }
+            else if (document.getElementById(mar[1]).innerHTML.length !== 0 && document.getElementById(mar[1]).innerHTML === document.getElementById(mar[2]).innerHTML && document.getElementById(mar[0]).innerHTML.length === 0) {
+              other = mar[0]
+              break;
+            }
+            else if (document.getElementById(mar[1]).innerHTML.length !== 0 && document.getElementById(mar[1]).innerHTML === document.getElementById(mar[0]).innerHTML && document.getElementById(mar[2]).innerHTML.length === 0) {
+              other = mar[2]
+              break;
+            }
+            else if (document.getElementById(mar[2]).innerHTML.length !== 0 && document.getElementById(mar[2]).innerHTML === document.getElementById(mar[0]).innerHTML && document.getElementById(mar[1]).innerHTML.length === 0) {
+              other = mar[1]
+              break;
+            }
+            else if (document.getElementById(mar[2]).innerHTML.length !== 0 && document.getElementById(mar[2]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML.length === 0) {
+              other = mar[0]
+              break;
+            }
+          }
+        }
+        return null
+      })
+      if (other) {
+        array.push(other)
+        document.getElementById(other).disabled = true
+        document.getElementById(other).innerHTML = 'O'
+      }
+    }
+    if (array.length > 8) {
+      document.getElementById('none').style.display = 'block'
+      document.getElementById('noneText').style.display = 'block'
+      document.getElementById('noneText').innerHTML = 'DRAW'
+    }
+    matchArray.map(mar => {
+      if (document.getElementById(mar[0]).innerHTML.length > 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML) {
+        document.getElementById('none').style.display = 'block'
+        document.getElementById('noneText').style.display = 'block'
+        document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML === 'O' && 'LOSER')
+      }
+      return null
+    })
+    matchArray.map(mar => {
+      if (document.getElementById(mar[0]).innerHTML.length > 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML) {
+        document.getElementById('none').style.display = 'block'
+        document.getElementById('noneText').style.display = 'block'
+        document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML === 'X' ? 'WINNER' : 'LOSER')
+      }
+      return null
+    })
+  }
+
+  impossible = (id) => {
+    count++
+    document.getElementById(id).disabled = true
+    if (array.length < 9) {
+      array.push(id)
+      document.getElementById(id).innerHTML = 'X'
+      let newArr = arr.filter(a => !array.includes(a))
+      let other = newArr[parseInt(Math.round(Math.random() * (newArr.length - 1)))]
+      let check = other
+      if (count === 1) {
+        other = newArr.includes(5) ? 5 : 1
+      }
+      else if (count === 2) {
+        let secondMatch = ['123', '456', '789', '147', '258', '369', '159', '357', '124', '236', '478', '689', '238', '467', '149', '347', '359']
+        for (let i = 0; i < secondMatch.length; i++) {
+          let mar = secondMatch[i]
+          let a = document.getElementById(mar[0]).innerHTML
+          let b = document.getElementById(mar[1]).innerHTML
+          let c = document.getElementById(mar[2]).innerHTML
+          if (a === 'X' && a === b && c.length === 0) {
             other = mar[2]
             break;
           }
-          else if (document.getElementById(mar[0]).innerHTML.length !== 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML && document.getElementById(mar[1]).innerHTML.length === 0) {
+          else if (a === 'X' && a === c && b.length === 0) {
             other = mar[1]
             break;
           }
-          else if (document.getElementById(mar[1]).innerHTML.length !== 0 && document.getElementById(mar[1]).innerHTML === document.getElementById(mar[2]).innerHTML && document.getElementById(mar[0]).innerHTML.length === 0) {
+          else if (b === 'X' && b === c && a.length === 0) {
             other = mar[0]
             break;
           }
-          else if (document.getElementById(mar[1]).innerHTML.length !== 0 && document.getElementById(mar[1]).innerHTML === document.getElementById(mar[0]).innerHTML && document.getElementById(mar[2]).innerHTML.length === 0) {
+          else if (b === 'X' && b === a && c.length === 0) {
             other = mar[2]
             break;
           }
-          else if (document.getElementById(mar[2]).innerHTML.length !== 0 && document.getElementById(mar[2]).innerHTML === document.getElementById(mar[0]).innerHTML && document.getElementById(mar[1]).innerHTML.length === 0) {
+          else if (c === 'X' && c === a && b.length === 0) {
             other = mar[1]
             break;
           }
-          else if (document.getElementById(mar[2]).innerHTML.length !== 0 && document.getElementById(mar[2]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML.length === 0) {
+          else if (c === 'X' && c === b && a.length === 0) {
             other = mar[0]
             break;
           }
         }
       }
-      return null
-    })
-    if (other) {
-      array.push(other)
-      document.getElementById(other).disabled = true
-      document.getElementById(other).innerHTML = 'O'
+      else if (count > 2) {
+        for (let i = 0; i < matchArray.length; i++) {
+          let mar = matchArray[i]
+          let a = document.getElementById(mar[0]).innerHTML
+          let b = document.getElementById(mar[1]).innerHTML
+          let c = document.getElementById(mar[2]).innerHTML
+          if (a === 'O' && a === b && c.length === 0) {
+            other = mar[2]
+            break;
+          }
+          else if (a === 'O' && a === c && b.length === 0) {
+            other = mar[1]
+            break;
+          }
+          else if (b === 'O' && b === c && a.length === 0) {
+            other = mar[0]
+            break;
+          }
+          else if (b === 'O' && b === a && c.length === 0) {
+            other = mar[2]
+            break;
+          }
+          else if (c === 'O' && c === a && b.length === 0) {
+            other = mar[1]
+            break;
+          }
+          else if (c === 'O' && c === b && a.length === 0) {
+            other = mar[0]
+            break;
+          }
+        }
+
+        if (check === other) {
+          for (let i = 0; i < matchArray.length; i++) {
+            let mar = matchArray[i]
+            let a = document.getElementById(mar[0]).innerHTML
+            let b = document.getElementById(mar[1]).innerHTML
+            let c = document.getElementById(mar[2]).innerHTML
+
+            if (a === 'X' && a === b && c.length === 0) {
+              other = mar[2]
+              break;
+            }
+            else if (a === 'X' && a === c && b.length === 0) {
+              other = mar[1]
+              break;
+            }
+            else if (b === 'X' && b === c && a.length === 0) {
+              other = mar[0]
+              break;
+            }
+            else if (b === 'X' && b === a && c.length === 0) {
+              other = mar[2]
+              break;
+            }
+            else if (c === 'X' && c === a && b.length === 0) {
+              other = mar[1]
+              break;
+            }
+            else if (c === 'X' && c === b && a.length === 0) {
+              other = mar[0]
+              break;
+            }
+          }
+        }
+      }
+      if (other) {
+        array.push(other)
+        document.getElementById(other).disabled = true
+        document.getElementById(other).innerHTML = 'O'
+      }
     }
-  }
-  matchArray.map(mar => {
-    if (document.getElementById(mar[0]).innerHTML.length > 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML) {
-      document.getElementById('none').style.display = 'block'
-      document.getElementById('noneText').style.display = 'block'
-      document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML === 'X' ? 'WINNER' : 'LOSER')
-    }
-    return null
-  })
-  if (array.length > 8) {
+    if (array.length > 8) {
       document.getElementById('none').style.display = 'block'
       document.getElementById('noneText').style.display = 'block'
       document.getElementById('noneText').innerHTML = 'DRAW'
+    }
+    matchArray.map(mar => {
+      if (document.getElementById(mar[0]).innerHTML.length > 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML) {
+        document.getElementById('none').style.display = 'block'
+        document.getElementById('noneText').style.display = 'block'
+        document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML === 'O' && 'LOSER')
+      }
+      return null
+    })
+    matchArray.map(mar => {
+      if (document.getElementById(mar[0]).innerHTML.length > 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML) {
+        document.getElementById('none').style.display = 'block'
+        document.getElementById('noneText').style.display = 'block'
+        document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML === 'X' ? 'WINNER' : 'LOSER')
+        console.log('Whaaaaaaaaaaaaat', document.getElementById(mar[0]).innerHTML, mar)
+      }
+      return null
+    })
+  }
+
+  two = (id) => {
+    this.setState({ reset: true })
+    count++
+    document.getElementById(id).disabled = true
+    if (array.length < 9) {
+      array.push(id)
+      if (array.length % 2 === 0) {
+        document.getElementById(id).innerHTML = 'O'
+      }
+      else {
+        document.getElementById(id).innerHTML = 'X'
+      }
+    }
+    if (array.length > 8) {
+      document.getElementById('none').style.display = 'block'
+      document.getElementById('noneText').style.display = 'block'
+      document.getElementById('noneText').innerHTML = 'DRAW'
+    }
+    matchArray.map(mar => {
+      if (document.getElementById(mar[0]).innerHTML.length > 0 && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[1]).innerHTML && document.getElementById(mar[0]).innerHTML === document.getElementById(mar[2]).innerHTML) {
+        document.getElementById('none').style.display = 'block'
+        document.getElementById('noneText').style.display = 'block'
+        document.getElementById('noneText').innerHTML = (document.getElementById(mar[0]).innerHTML === 'X' ? 'X WON' : 'O WON')
+      }
+      return null
+    })
+
+  }
+  handleSelect = (event) => {
+    this.setState({ selected: event.target.value })
+  }
+  state = {
+    selected: 'easy',
+    reset: false
+  }
+  tictactoe = (item) => {
+    this.state.selected === 'easy' ? this.easy(item) : this.state.selected === 'hard' ? this.hard(item) : this.state.selected === 'impossible' ? this.impossible(item) : this.two(item)
+  }
+  reset = () => {
+    array = []
+    check = []
+    count = 0
+    document.getElementById('none').style.display = 'none'
+    document.getElementById('noneText').style.display = 'none'
+    arr.map(id => {
+      document.getElementById(id).innerHTML = ''
+      document.getElementById(id).disabled = false
+    })
+  }
+  render() {
+    return (
+      <div className={this.state.selected}>
+        <span className='header'>Tic Tac Toe</span>
+        <div className='outer'>
+          <div className='grid-box'>
+            {arr.map(item =>
+              <button onClick={() => this.tictactoe(item)} id={item} key={item} />
+            )}
+          </div>
+        </div>
+        <div className='my-flex'>
+          <select value={this.state.selected} className='selective' onChange={this.handleSelect}>
+            <option value='easy'>Easy</option>
+            <option value='hard'>Hard</option>
+            <option value='impossible'>Impossible</option>
+            <option value='two'>Two Players</option>
+          </select>
+          <button className='refresh' onClick={this.reset}>RESET</button>
+        </div>
+        <span id='none' style={{ display: 'none' }} onClick={this.reset} />
+        <span id='noneText' style={{ display: 'none' }} onClick={this.reset} />
+        <i className='watermark'>Raghuveer Bharadwaj</i>
+      </div>
+    )
   }
 }
-function App() {
-  return (
-    <div>
-      <span className='header'>Tic Tac Toe</span>
-      <div className='grid-box'>
-        {arr.map(item =>
-          <button onClick={() => tictactoe(item)} id={item} key={item} />
-        )}
-      </div>
-      <button className='refresh' onClick={() => window.location.reload()}>RESET GAME</button>
-      <span id='none' style={{ display: 'none' }} onClick={() => window.location.reload()} />
-      <span id='noneText' style={{ display: 'none' }} onClick={() => window.location.reload()} />
-      <i className = 'watermark'>Raghuveer Bharadwaj</i>
-    </div>
-  )
-}
 
-export default App;
+export default App
