e1b148d148ad961f3b9f75de4c1e80bfc7aed417
Raghuveer Bharadwaj <raghuveer.mh@cognitiveclouds.com>
2020-08-16 16:37:22 +0530

    Updates

---
+++ precache-manifest.8fe50f02c77d7b946ef46d927ea2ef69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56588759298864df6f55fba2700355c4",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "91c54993f83000b7f6b7",
+    "url": "/TicTacToe/static/css/main.db73054c.chunk.css"
+  },
+  {
+    "revision": "0a4e84612a5651937d75",
+    "url": "/TicTacToe/static/js/2.17053863.chunk.js"
+  },
+  {
+    "revision": "91c54993f83000b7f6b7",
+    "url": "/TicTacToe/static/js/main.b24e1145.chunk.js"
+  },
+  {
+    "revision": "f893c29da8654d2a4928",
+    "url": "/TicTacToe/static/js/runtime-main.b64623c4.js"
+  }
+]);
